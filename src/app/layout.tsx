924a8a358976b0d5e04b483509ce9f4573374d44
anakavtaradze1 <anakavtaradze1@gmail.com>
2026-02-16 16:52:07 +0400

    add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar/navbar";
 import StoreProvider from "./StoreProvider";
 import Toast from "@/components/Toast/Toast";
 import Footer from "@/components/footer/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const rubik = Rubik({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -35,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <StoreProvider>
-        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <body className={rubik.className}>
           <Navbar />
           <Toast />
           {children}
